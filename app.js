2585283281e284af3b8c98b259b93cefce3bab1d
Alex Douglas <alex.netley@gmail.com>
2017-02-27 12:50:00 +0000

    Prototype 1

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var request = require('request');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url);
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
@@ -19,55 +19,60 @@ var connector = new builder.ChatConnector({
 });
 
 var bot = new builder.UniversalBot(connector, [
-    function (session) {
+    function(session) {
         builder.Prompts.text(session, "Hello... Which city are you looking in?");
     },
-    function (session, results) {
+    function(session, results) {
         session.privateConversationData.city = results.response;
         builder.Prompts.number(session, "Ok, you are looking in " + results.response + ", How many bedrooms are you looking for?");
     },
-    function (session, results) {
+    function(session, results) {
         session.privateConversationData.bedrooms = results.response;
         builder.Prompts.number(session, "You are looking for " + results.response + " bedrooms, what is the max price per month you want to pay?");
     },
-    function (session, results) {
+    function(session, results) {
         session.privateConversationData.ppm = results.response;
-        builder.Prompts.text(session, "Your max rent is " + results.response + " per month, woulld you like a furnished place?");
+        builder.Prompts.text(session, "Your max rent is " + results.response + " per month, woulld you like bills included?");
     },
-    function (session, results) {
+    function(session, results) {
         if (results.response == "yes") {
-          session.privateConversationData.isFurnished = 1;
-        }
-        else {
-          session.privateConversationData.isFurnished = 0;
+            session.privateConversationData.bills = 1;
+        } else {
+            session.privateConversationData.bills = 0;
         }
         session.send("You are looking in " + session.privateConversationData.city +
-                     "Your number of bedrooms is " + session.privateConversationData.bedrooms +
-                     "Your price per month is " + session.privateConversationData.ppm +
-                     "Your furnishings is " + session.privateConversationData.isFurnished);
-        var url = "http://localhost:5000/accommodation/bristol/5/400/1";
+            "Your number of bedrooms is " + session.privateConversationData.bedrooms +
+            "Your price per month is " + session.privateConversationData.ppm +
+            "Your furnishings is " + session.privateConversationData.bills);
+        var url = "http://localhost:5000/accommodation/" + session.privateConversationData.city + "/" + session.privateConversationData.bedrooms.toString() +
+            "/" + session.privateConversationData.ppm.toString() + "/1/" + session.privateConversationData.bills.toString();
 
-        request(url, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var data = JSON.parse(body);
+        request(url, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var data = JSON.parse(body);
 
-          }
+                var cards = makeCards(session, data);
+                var reply = new builder.Message(session)
+                    .attachmentLayout(builder.AttachmentLayout.carousel)
+                    .attachments(cards);
+                session.send(reply);
+            }
         });
-        var msg = new builder.Message(session)
-            .textFormat(builder.TextFormat.xml)
-            .attachments([
-                new builder.HeroCard(session)
-                    .title("Hero Card")
-                    .subtitle("Space Needle")
-                    .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
-                    .images([
-                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-                    ])
-                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
-            ]);
-            session.send(msg);
+
     }
 ]);
+
+function makeCards(session, data) {
+    var cards = [];
+    for (var i = 0; i < data.length; i++) {
+        cards.push(new builder.HeroCard(session)
+            .title(data[i].bedrooms + " bedroom house in " + data[i].address)
+            .subtitle("£" + data[i].ppm + " per month")
+            .text("Tap or click to open the house listing for more info.")
+            .tap(builder.CardAction.openUrl(session, data[i].url)));
+    }
+    return cards;
+}
 
 server.post('/api/messages', connector.listen());
 
